c976f656678e3974825d9bdaeb5c06b62770a335
Motko <77466238+Motko222@users.noreply.github.com>
2024-12-12 10:49:44 +0100

    Update report.sh

--- report.sh
+++ report.sh
@@ -8,7 +8,7 @@ source ~/.bash_profile
 version=$(echo ?)
 service=$(sudo systemctl status $folder --no-pager | grep "active (running)" | wc -l)
 errors=$(journalctl -n 1000 -u $folder.service --no-hostname -o cat | grep -c -E "rror|ERR")
-prover_identifier=$(journalctl -u $folder.service --no-hostname -o cat | grep "Your current prover identifier is " | tail -1 | awk -F "Your current prover identifier is " '{print $NF}' | cut -d \) -f 1 )
+prover_identifier=$(cat /root/.nexus/prover-id)
 url=
 
 status="ok"
