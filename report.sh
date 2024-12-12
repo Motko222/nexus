e36a8b24c6f4c153dbe846f038c52fd41e55d276
Motko <77466238+Motko222@users.noreply.github.com>
2024-12-12 10:18:21 +0100

    Create report.sh

---
+++ report.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+json=~/logs/report-$folder
+source ~/.bash_profile
+
+version=$(echo ?)
+service=$(sudo systemctl status $folder --no-pager | grep "active (running)" | wc -l)
+errors=$(journalctl -u $folder.service --no-hostname -o cat | grep $(date --utc +%F) | grep -c -E "rror|ERR")
+prover_identifier=$(journalctl -u $folder.service --no-hostname -o cat | grep "Your current prover identifier is " | tail -1 | awk -F "Your current prover identifier is " '{print $NF}' | cut -d \) -f 1 )
+url=
+
+status="ok"
+[ $service -ne 1 ] && status="error";message="service not running";
+[ $errors -gt 20 ] && status="warning";message="errors=$errors";
+
+cat >$json << EOF
+{
+  "updated":"$(date --utc +%FT%TZ)",
+  "measurement":"report",
+  "tags": {
+       "id":"$folder",
+       "machine":"$MACHINE",
+       "grp":"node",
+       "owner":"$OWNER"
+  },
+  "fields": {
+        "chain":"testnet",
+        "network":"testnet",
+        "version":"$version",
+        "status":"$status",
+        "message":"$message",
+        "service":$service,
+        "errors":$errors,
+        "prover_identifier":"$prover_identifier",
+        "url":"$url"
+  }
+}
+EOF
+
+cat $json | jq
