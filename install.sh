6626f56a4857c8cf788e551d514c9ebe3be331f1
Motko <77466238+Motko222@users.noreply.github.com>
2024-12-12 17:01:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 
 apt update && sudo apt upgrade
-apt install curl build-essential pkg-config libssl-dev git-all -y
+apt install curl build-essential pkg-config libssl-dev git-all protobuf-compiler -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs/ | sh
 source $HOME/.cargo/env
 rustc --version
