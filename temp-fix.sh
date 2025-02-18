4aa08c76e3b40d4f1abf600d7ec1cdb4422b4b4e
Motko <77466238+Motko222@users.noreply.github.com>
2025-02-19 00:06:10 +0100

    Create temp-fix.sh

---
+++ temp-fix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source /root/.cargo/env
+sudo apt remove -y protobuf-compiler 
+curl -LO https://github.com/protocolbuffers/protobuf/releases/download/v25.2/protoc-25.2-linux-x86_64.zip 
+unzip protoc-25.2-linux-x86_64.zip -d $HOME/.local 
+export PATH="/root/.local/bin:$PATH" 
+protoc --version
+cd /root/.nexus/network-api/clients/cli
+rustup target add riscv32i-unknown-none-elf
