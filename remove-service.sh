88c31f7e6ddb12d898b0fea3637dd147889ed2c3
Motko <77466238+Motko222@users.noreply.github.com>
2025-02-19 20:17:42 +0100

    Update remove-service.sh

--- remove-service.sh
+++ remove-service.sh
@@ -3,6 +3,9 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
 folder=$(echo $path | awk -F/ '{print $NF}')
 
+read -p "Sure? " c
+case $c in y|Y) ;; *) exit ;; esac
+
 systemctl stop $folder.service
 systemctl disable $folder.service
 rm /etc/systemd/system/$folder.service
