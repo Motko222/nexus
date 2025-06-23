b5c6e27f33741f6d75c55887939d38e05b89243c
Motko <77466238+Motko222@users.noreply.github.com>
2025-06-23 20:53:59 +0200

    Update install-binary.sh

--- install-binary.sh
+++ install-binary.sh
@@ -2,6 +2,7 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 
 cd /root
+[ -d cd nexus-cli ] && rm -r nexus-cli
 git clone https://github.com/nexus-xyz/nexus-cli.git
 cd nexus-cli/clients/cli
 cargo build --release
