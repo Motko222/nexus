e5acb7f7bc63f51fb2d52e4246a44888e6e2e4fc
Motko <77466238+Motko222@users.noreply.github.com>
2025-06-23 16:37:23 +0200

    Create install-binary.sh

---
+++ install-binary.sh
@@ -0,0 +1,12 @@
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
+folder=$(echo $path | awk -F/ '{print $NF}')
+
+cd /root
+git clone https://github.com/nexus-xyz/nexus-cli.git
+cd nexus-cli/clients/cli
+cargo build --release
+
+#create env
+cd $path
+[ -f env ] || cp env.sample env
+nano env
