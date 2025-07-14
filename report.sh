4e2fd92d17d5e134da1ecb67643deda06ec6d320
Motko <77466238+Motko222@users.noreply.github.com>
2025-07-14 16:07:30 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -6,10 +6,10 @@ json=~/logs/report-$folder
 source ~/.bash_profile
 source $path/env
 
-version=$(/root/nexus-cli/clients/cli/target/release/nexus-network -V | awk '{print $NF}')
+version=$(/root/.nexus/bin/nexus-cli -V | awk '{print $NF}')
 service=$(sudo systemctl status $folder --no-pager | grep "active (running)" | wc -l)
 errors=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -c -E "rror|ERR")
-success=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -c "Successfully submitted proof")
+success=$(journalctl -u $folder.service --since "1 hour ago" --no-hostname -o cat | grep -c "Proof submitted")
 
 status="ok";message="";
 [ $errors -gt 500 ] && status="warning" && message="too many errors";
